ef5b1d4179c618bc69d0c0f5ce3a06a2c0a460fd
risingmoon77 <robvandam_786@hotmail.com>
2018-11-20 09:36:18 +0000

    typo

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import ExpenseForm from './Expenseform';
+import ExpenseForm from './ExpenseForm';
 import { editExpense } from '../actions/expenses';
 import { removeExpense } from '../actions/expenses';
 
