45404cdb7f2dbce1c19aa2bd3731d3b21cf381b9
risingmoon77 <robvandam_786@hotmail.com>
2019-01-03 20:43:56 +0000

    Added Expensify styles

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -28,19 +28,28 @@ export class ExpenseListFilters extends React.Component {
   };
   render() {
     return (
-      <div>
-        <input
-          type="text"
-          value={this.props.filters.text}
-          onChange={this.onTextChange}
-        />
+      <div className="content-container">
+        <div className="input-group">
+          <div className="input-group__item">  
+            <input
+              type="text"
+              className="text-input"
+              placeholder="Search expenses"
+              value={this.props.filters.text}
+              onChange={this.onTextChange}
+            />
+        </div>
+        <div className="input-group__item">  
         <select
+          className="select"
           value={this.props.filters.sortBy}
           onChange={this.onSortChange }
         >
           <option value="date">Date</option>
           <option value="amount">Amount</option>
         </select>
+        </div>
+        <div className="input-group__item">  
         <DateRangePicker
           startDate={this.props.filters.startDate}
           endDate={this.props.filters.endDate}
@@ -51,7 +60,12 @@ export class ExpenseListFilters extends React.Component {
           numberOfMonths={1}
           isOutsideRange={() => false}
         />
+       </div>
       </div>
+        </div>
+      
+     
+     
     );
   }
 };
--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpenses  from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
@@ -9,8 +10,13 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal}) => {
     const expenseFormat = numeral(expensesTotal / 100).format('$0,0.00');
 
     return (
-        <div>
-        <h2>Viewing {expenseCount} {expenseWord} totalling {expenseFormat}</h2>
+        <div className="page-header">
+            <div className="content-container">
+              <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{expenseFormat}</span></h1>
+              <div className="page-header__actions">
+                <Link className="button" to="/create">Add Expense</Link>
+              </div>
+            </div>
         </div>
         );
 };
@@ -18,9 +24,9 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal}) => {
 const mapStateToProps = (state) => {
     const visibleExpenses = selectExpenses(state.expenses, state.filters);
     return {
-            expenseCount: visibleExpenses.length,
-            expensesTotal: selectExpensesTotal(visibleExpenses)
-        };
+        expenseCount: visibleExpenses.length,
+        expensesTotal: selectExpensesTotal(visibleExpenses)
+    };
 };
 
 export default connect(mapStateToProps)(ExpensesSummary);
